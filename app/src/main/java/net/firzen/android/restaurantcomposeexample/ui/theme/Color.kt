34211556be84a8f319dbe495901d347962f307cb
Firzen7 <firzen.developer@gmail.com>
2026-01-18 18:03:23 +0100

    Better background color

--- app/src/main/java/net/firzen/android/restaurantcomposeexample/ui/theme/Color.kt
+++ app/src/main/java/net/firzen/android/restaurantcomposeexample/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val MatrixBlack = Color(0xFF14241B)
+val MatrixBlack = Color(0xFF475047)
 val MatrixSurface = Color(0xFF121A14)
 
 val MatrixGreenPrimary = Color(0xFF00FF6A)
