a0fdddbca7d0c93e26dff0f3805d77ac434fad22
Firzen7 <firzen.developer@gmail.com>
2026-02-10 20:08:20 +0100

    Added first UI test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,11 +84,14 @@ dependencies {
     // allows DI inside of composable functions
     kapt("com.google.dagger:hilt-compiler:$hilt_version")
 
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
